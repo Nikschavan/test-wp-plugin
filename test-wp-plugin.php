4b921a45e9850f830409a255e894ec24c19eaf09
nikhilc@bsf.io <nikhilc@bsf.io>
2016-02-09 20:39:04 +0530

    First commit

---
+++ test-wp-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: TEST PLUGIN
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: test-wp-plugin
+ * Domain Path: /languages
+ * @package TEST PLUGIN
+ */
+
+
+
