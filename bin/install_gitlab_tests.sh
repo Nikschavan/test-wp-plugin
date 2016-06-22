125fb2f77dd8bd8490d3b1baa5b37f08b5cb5323
nikhilc@bsf.io <nikhilc@bsf.io>
2016-06-22 19:29:30 +0530

    revert limited packages for running tests

--- bin/install_gitlab_tests.sh
+++ bin/install_gitlab_tests.sh
@@ -106,28 +106,16 @@ install_gitlab_test_runner() {
 	# Remove contents of /var/lib/apt/lists to fix "Hash sum mismatch" error
 	# rm -rf /var/lib/apt/lists/*
 
-	# apt-get -yqq update
-	# apt-get -yqq --fix-missing upgrade
+	apt-get -yqq update
+	apt-get -yqq --fix-missing upgrade
 
-	# # instll the required packages for the running CI tests
-	# apt-get -yqq install git zip unzip subversion libapache2-svn
-	# apt-get install -y phpunit php5-mysql 
+	# instll the required packages for the running CI tests
+	apt-get -yqq install git zip unzip subversion libapache2-svn
+	apt-get install -y phpunit php5-mysql mysql-client libmysqlclient-dev
 
-	# Update packages and install composer and PHP dependencies.
-	apt-get update -yqq
-	apt-get install git phpunit libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
-
-	# Compile PHP, include these extensions.
-	docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
-
-	# # mysql-client libmysqlclient-dev
-
-	# # Install mysql driver
-	# docker-php-ext-install mysqli	
-	# docker-php-ext-install pdo pdo_mysql
-
-	# service mysql status
-	# service mysql start
+	# Install mysql driver
+	docker-php-ext-install mysqli	
+	docker-php-ext-install pdo pdo_mysql
 }
 
 install_gitlab_test_runner
